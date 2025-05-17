dbd27a88a49f14afd5699766a19f6dbebb59b026
just-fabian <fabian130404@gmail.com>
2025-05-17 07:14:18 -0400

    SPACIO-125 feat: update adapters to environments and object detection services

--- src/Application/Commands/Concretes/UploadTour360Command.cs
+++ src/Application/Commands/Concretes/UploadTour360Command.cs
@@ -1,6 +1,7 @@
 using AdminService.Src.Application.Commands.Interfaces;
 using AdminService.Src.Application.DTOs.Create;
 using AdminService.Src.Application.Interfaces;
+using AdminService.Src.Domain.Enums;
 using AdminService.Src.Domain.Interfaces;
 
 namespace AdminService.Src.Application.Commands.Concretes;
@@ -9,27 +10,32 @@ public class UploadTour360Command(
     Guid tourRequestId,
     TourUploadDto tourUpload,
     ITour360RequestRepository repository,
-    ITourUploaderAdapter tourUploaderAdapter) : ICommand<bool>
+    ITourUploaderAdapter tourUploaderAdapter,
+    IObjectDetectionAdapter objectDetectionAdapter,
+    IEnvironmentServiceAdapter environmentServiceAdapter
+) : ICommand<bool>
 {
     private readonly Guid _tourRequestId = tourRequestId;
     private readonly TourUploadDto _tourUpload = tourUpload;
     private readonly ITour360RequestRepository _repository = repository;
     private readonly ITourUploaderAdapter _tourUploaderAdapter = tourUploaderAdapter;
+    private readonly IObjectDetectionAdapter _objectDetectionAdapter = objectDetectionAdapter;
+    private readonly IEnvironmentServiceAdapter _environmentServiceAdapter = environmentServiceAdapter;
 
     public async Task<bool> ExecuteAsync()
     {
         var tourRequest = await _repository.GetByIdAsync(_tourRequestId)
                           ?? throw new Exception("360 Tour Request Not Found");
 
-        if (tourRequest.Status != Domain.Enums.Tour360Status.Pending &&
-            tourRequest.Status != Domain.Enums.Tour360Status.Scheduled)
+        if (tourRequest.Status != Tour360Status.Pending &&
+            tourRequest.Status != Tour360Status.Scheduled)
         {
             throw new Exception("The Request cannot be Uploaded in Current Status");
         }
 
         await _tourUploaderAdapter.UploadTourAsync(tourRequest.EnvironmentId, _tourUpload);
 
-        tourRequest.Status = Domain.Enums.Tour360Status.Completed;
+        tourRequest.Status = Tour360Status.Completed;
         await _repository.UpdateAsync(tourRequest);
 
         return true;
